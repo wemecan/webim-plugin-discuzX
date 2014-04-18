fff87f2a9025be214acf727e5a56ecb85726f5ae
Ery Lee <ery.lee@gmail.com>
2014-04-18 18:20:27 +0800

    , -> ;

--- webim_plugin_discuzx.class.php
+++ webim_plugin_discuzx.class.php
@@ -64,7 +64,7 @@ class webim_plugin_discuzX extends webim_plugin {
         $vid = $this->vid($id);
         $data = DB::fetch_first("SELECT id from ".DB::table('webim_visitors')." WHERE name = '$vid'");
         $ip = isset($_SERVER['X-Forwarded-For']) ? $_SERVER['X-Forwarded-For'] : $_SERVER["REMOTE_ADDR"];
-        $loc = implode('', IP::find($ip)),
+        $loc = implode('',  IP::find($ip) );
         if( !($data && $data["id"]) ) {
             //var_dump($_SERVER);
             DB::insert('webim_visitors', array(
